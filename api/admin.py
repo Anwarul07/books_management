aebce2906dc5113a9013893aafd2b7d5d1e9b5ad
Anwarul07 <Officialmdanwarulhaque@gmail.com>
2025-11-02 16:49:03 +0530

    created admin configuration for author and category

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,51 @@
 from django.contrib import admin
-from .models import Books
+from .models import Books, Category, Author, Cart, Wishlist
 
 
+# Category model admin configuration
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "category_name",
+        "description",
+        "cover_image",
+        "cover_front",
+        "cover_behind",
+        "cover_top",
+        "cover_bottom",
+        "cover_side",
+        "origin",
+    ]
+    list_filter = ["category_name", "origin"]
+    search_fields = ["category_name", "description"]
+    ordering = ["category_name"]
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "author_name",
+        "email",
+        "cover_image",
+        "cover_front",
+        "cover_behind",
+        "cover_top",
+        "cover_bottom",
+        "cover_side",
+        "biography",
+        "is_verified",
+        "register_date",
+        "contact",
+        "Date_of_Birth",
+        "short_description",
+    ]
+    list_filter = ["author_name", "is_verified"]
+    search_fields = ["author_name", "email"]
+    ordering = ["author_name"]
+    
+
+# Book model admin configuration
 @admin.register(Books)
 class BooksAdmin(admin.ModelAdmin):
     list_display = [
