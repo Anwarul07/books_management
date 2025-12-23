3e257e69289523a2fd7aa3cdcacf9cf9d2b139d2
Anwarul07 <Officialmdanwarulhaque@gmail.com>
2025-12-24 01:29:29 +0530

    removed to clodunary to local back

--- api/urls.py
+++ api/urls.py
@@ -39,5 +39,5 @@ urlpatterns = [
 ]
 
 
-# if settings.DEBUG:  # Serve media files during development
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:  # Serve media files during development
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
